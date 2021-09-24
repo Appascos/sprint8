052d070cd39f8dab929ceae4ce87d654b66514f4
unknown <gloriazoldano@gmail.com>
2021-09-24 02:15:07 -0300

    Dashboard funcionando

---
+++ yotemudo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './assets/css/app.css';
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <main>
+        <Dashboard />
+      </main>
+    </div>
+  );
+}
+
+export default App;
